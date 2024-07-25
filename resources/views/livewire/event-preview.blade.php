7b06c4977e725b992891bcb4660d02b03832d0da
Erik Hanson <erik@erikhanson.dev>
2024-07-25 15:35:16 -0700

    Remove first() call from events in preview

--- resources/views/livewire/event-preview.blade.php
+++ resources/views/livewire/event-preview.blade.php
@@ -1,21 +1,21 @@
 <main class="container">
-    @section('title', $event->first()->name)
+    @section('title', $event->name)
     <article>
         <header>
             <hgroup>
-                <h1>{{ $event->first()->name }}</h1>
+                <h1>{{ $event->name }}</h1>
                 <h2>
-                    📅  {{ $event->first()->start_time->format('F j, Y') }},
-                    {{ $event->first()->start_time->format('g:i a') }} -
-                    {{ $event->first()->end_time->format('g:i a') }}
+                    📅  {{ $event->start_time->format('F j, Y') }},
+                    {{ $event->start_time->format('g:i a') }} -
+                    {{ $event->end_time->format('g:i a') }}
                     <br/>
-                    📍  {{ $event->first()->location }}
+                    📍  {{ $event->location }}
                 </h2>
             </hgroup>
 
             <p>Heya 👋</p>
 
-            <p>{!! nl2br(e($event->first()->description)) !!}</p>
+            <p>{!! nl2br(e($event->description)) !!}</p>
         </header>
         <p>ℹ️  This is an event preview.</p>
         <p>If this were an actual invite, the RSVP form would be here! 👇</p>
