85c04ad9f4852206fc5cddf4742e0cbee874a30f
Erik Hanson <erik@erikhanson.dev>
2024-07-26 10:27:13 -0700

    Add Line notification bot

--- app/Models/Invite.php
+++ app/Models/Invite.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
+use LINE\Clients\MessagingApi\Model\PushMessageRequest;
+use LINE\Clients\MessagingApi\Model\TextMessage;
+use LINE\Laravel\Facades\LINEMessagingApi;
 use Symfony\Component\Uid\Ulid;
+use function Illuminate\Events\queueable;
 
 /**
  * @property Ulid $id
@@ -42,6 +47,23 @@ class Invite extends Model
                 (new SendInvite($invite))->execute();
             }
         });
+
+        static::saved(queueable(function (Invite $invite) {
+            $original = $invite->getOriginal();
+            if ($invite->name !== $original['name'] || $invite->email !== $original['email']) {
+                return;
+            }
+
+            try {
+                $event = $invite->event()->first();
+                $text = '🎟️ Update for "' . $event->name . '":'. PHP_EOL . PHP_EOL . ($invite->is_attending ? '✅ ' : '❌ ') . $invite->name . ($invite->is_attending ? ' is attending!' : ' cannot attend.') . PHP_EOL . PHP_EOL . 'See: ' . url('admin/events/' . $invite->event_id);
+                $message = new TextMessage(['type' => 'text', 'text' => $text]);
+                $request =  new PushMessageRequest(['to' => config('line-bot.user_id'), 'messages' => [$message]]);
+                LINEMessagingApi::pushMessage($request);
+            } catch (\Exception $e) {
+                Log::error("Couldn't send LINE message: " . $e->getMessage());
+            }
+        }));
     }
 
     public function event(): BelongsTo
